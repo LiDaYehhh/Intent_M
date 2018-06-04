80d8b29dfb412bf3d1e398c55b972c017ebf96ea
LiDaYehhh <37533522+LiDaYehhh@users.noreply.github.com>
2018-06-04 21:37:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/elaine/intent/NewActivity.java
@@ -0,0 +1,15 @@
+package com.example.elaine.intent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+    }
+
+}
